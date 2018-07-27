b9e47e5b76ecd40916a2924a5a5bf0320511a00c
Jash Mota <jashmota101@gmail.com>
2018-07-27 15:36:31 +0530

    Create allbitsflash.asm

---
+++ allbitsflash.asm
@@ -0,0 +1,16 @@
+start:
+mov p1, #0ffh
+call delay
+mov p1, #00h
+call delay
+sjmp start
+
+delay:
+mov r2, #0ah
+loop1: mov r1, #0c8h
+loop0: mov r0, #0ffh
+wait: djnz r0, wait
+	djnz r1, loop0
+	djnz r2, loop1
+reti
+end
